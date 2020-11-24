0c230e623b7abe19c7ebc134ae08648aed0e1b31
KevinHerrera <kaherrerag@eafit.edu.co>
2020-11-24 18:38:17 -0500

    Crout

--- LUFactorization/views.py
+++ LUFactorization/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from Metodos.SimpleLU import simpleLU
 from Metodos.PartialLU import partialLU
 from Metodos.doolittle import doolittlee
+from Metodos.crout import croutt
 # Create your views here.
 
 
@@ -53,3 +54,27 @@ def doolittle(request):
         except:
             return render(request, "doolittle.html", {'data': ''})
     return render(request, "doolittle.html",{'data':''})
+
+
+def crout(request):
+    if 'n' in request.POST:
+        n = int(request.POST.get('n'))
+        matrix = []
+        b = []
+        for i in range(n):
+            fila = []
+            b.append(float(request.POST.get('vector'+str(i))))
+            for j in range(n):
+                fila.append(float(request.POST.get('matrix'+str(i)+str(j))))
+            matrix.append(fila)
+        print(matrix)
+        print(b)
+        try:
+            print(2)
+            result = croutt(matrix, b)
+            print('-------------------')
+            print(result)
+            return render(request, "crout.html", {'x': result[0], 'L': result[1], 'U': result[2],'message':result[3]})
+        except:
+            return render(request, "crout.html", {'data': ''})
+    return render(request, "crout.html",{'data':''})
